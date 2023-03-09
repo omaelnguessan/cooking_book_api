4df0bc6a850ff5003855c75b5865f410f2c864fa
madaraUchiwa <omaelberiz@outlook.fr>
2023-03-09 10:21:24 +0000

    first commit

---
+++ src/test/java/com/omael/cookingbook/CookingbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omael.cookingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
