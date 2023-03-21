1b4bf8c1cc8894e0fb717f28b0d95ea9163f47c7
omael <omael.nguessan@olympemedia.com>
2023-03-21 11:00:49 +0000

    fix(dao): update RowMapper for setCategory and setUser

--- src/main/java/com/omael/cookingbook/recipe/dao/RecipeRowMapper.java
+++ src/main/java/com/omael/cookingbook/recipe/dao/RecipeRowMapper.java
@@ -1,7 +1,9 @@
 package com.omael.cookingbook.recipe.dao;
 
+import com.omael.cookingbook.category.Category;
 import com.omael.cookingbook.recipe.Recipe;
 import com.omael.cookingbook.recipe.RecipeLevel;
+import com.omael.cookingbook.user.User;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -14,12 +16,19 @@ public class RecipeRowMapper implements RowMapper<Recipe> {
         recipe.setId(rs.getInt("id"));
         recipe.setName(rs.getString("name"));
         recipe.setDescription(rs.getString("description"));
-        recipe.setRecipeLevel(RecipeLevel.values()[rs.getInt("recipeLevel")]);
-        recipe.setPrepareTime(rs.getInt("prepareTime"));
-        recipe.setCookingTime(rs.getInt("cookingTime"));
-        recipe.setTotalTime(rs.getInt("totalTime"));
-        recipe.getCategory().setId(rs.getInt("categoryId"));
-        recipe.getUser().setId(rs.getInt("userId"));
+        recipe.setRecipeLevel(RecipeLevel.valueOf(rs.getString("recipe_level")));
+        recipe.setPrepareTime(rs.getInt("prepare_time"));
+        recipe.setCookingTime(rs.getInt("cooking_time"));
+        recipe.setTotalTime(rs.getInt("total_time"));
+
+        User user = new User();
+        user.setId((rs.getInt("user_id")));
+        recipe.setUser(user);
+
+        Category category = new Category();
+        category.setId(rs.getInt("category_id"));
+        recipe.setCategory(category);
+
         return recipe;
     }
 }
